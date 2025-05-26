f2168a19d8f2286fa356e5f1f9549db70b41bf14
Timothy Hobbs <thobbsinteractive@gmail.com>
2025-05-26 23:55:36 +0200

    fix: fixed declaration

--- remc2/engine/EventDispatcher.h
+++ remc2/engine/EventDispatcher.h
@@ -9,10 +9,34 @@ class EventDispatcher
 public:
 	static EventDispatcher* I;
 
-	~EventDispatcher();
-	void RegisterEvent(IEvent* event);
+	~EventDispatcher()
+	{
+		for (auto el : _eventList)
+		{
+			for (auto e : el.second)
+				delete e;
+		}
+	};
+
+	void RegisterEvent(IEvent* event)
+	{
+		if (event)
+			_eventList[event->GetType()].push_back(event);
+	};
+
 	template <typename ..._args>
-	void DispatchEvent(EventType eventType, _args...a);
+	void DispatchEvent(EventType eventType, _args...a)
+	{
+		auto it_eventList = _eventList.find(eventType);
+		if (it_eventList == _eventList.end())
+			return;
+
+		for (auto ie : it_eventList->second)
+		{
+			if (Event<_args...>* event = dynamic_cast<Event<_args...>*>(ie))
+				event->Trigger(a...);
+		}
+	};
 
 private:
 	std::map<EventType, std::vector<IEvent*>> _eventList;
