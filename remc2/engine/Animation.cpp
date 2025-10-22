e6295aebc2e2ce1b7a9a3075ea80d61ab414ec0d
Timothy Hobbs <thobbsinteractive@gmail.com>
2025-10-22 20:48:29 +0200

    refactor: more renaming
    (cherry picked from commit 79d2bff509629386ca23ef19c8f0b0f3a10820e8)

--- remc2/engine/Animation.cpp
+++ remc2/engine/Animation.cpp
@@ -30,7 +30,7 @@ int x_DWORD_17D730; // weak
 
 uint32_t x_DWORD_17D720[4]; // weak 0
 
-int16_t x_WORD_17D724; // weak 4
+uint16_t x_WORD_17D724; // weak 4
 uint16_t x_WORD_17D726; // weak 6
 
 char x_BYTE_17D738[256]; // idb
@@ -214,7 +214,7 @@ void sub_75DB0()//256db0
 	DataFileIO::Read(x_DWORD_17DB38_intro_file_handle, (uint8_t*)x_DWORD_17D720, 16);
 	x_WORD_17D724 = x_DWORD_17D720[1] & 0xffff;
 	x_WORD_17D726 = (x_DWORD_17D720[1] & 0xffff0000) >> 16;
-	while (x_WORD_17D724 != -3590)
+	while (x_WORD_17D724 != 0xf1fa/*-3590*/)
 		Logger->error("ERROR UNKNOWN FRAME TYPE");
 	DataFileIO::Read(x_DWORD_17DB38_intro_file_handle, x_DWORD_E9C38_smalltit, x_DWORD_17D720[0] - 16);
 	x_DWORD_E1300 += x_DWORD_17D720[0];
@@ -254,13 +254,13 @@ void /*__fastcall*/ sub_75E70()//256e70
 	v23 = 0;
 	x_DWORD_17DB50 = x_DWORD_E9C38_smalltit;
 	x_BYTE_17D738[0] = 0;
-	if (x_WORD_17D724 == -3840)
+	if (x_WORD_17D724 == 0xf100)
 	{
 		sub_75D70(0, x_DWORD_17D720[0] - 16);
 		/*v1 = */sub_75DB0();
 		/*a1 = 0;*/sub_75E70(/*v1*/);
 	}
-	else if (x_WORD_17D724 == -3590)
+	else if (x_WORD_17D724 == 0xF1FA)
 	{
 		v2 = 0;
 		while (1)
--- remc2/engine/Basic.cpp
+++ remc2/engine/Basic.cpp
@@ -445,7 +445,7 @@ Pathstruct xadatatables = { "",(uint8_t**)&x_DWORD_D41BC_langbuffer,&LANG_BEGIN_
 //#define psxazero14 47
 
 int GameTimerTick_17DB54; // weak
-uint32_t Unk_F4240 = 1000000; //CPU Frequency?
+uint32_t PitFrequency_F4240 = 1193182; //PIT Frequency
 
 bool IsDefaultResolution(int width, int height)
 {
