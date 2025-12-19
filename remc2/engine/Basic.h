6251fc788df24b92da702890d57764fb6201d372
Tomáš Veselý <turican0@gmail.com>
2025-12-19 13:13:18 +0100

    separate spells

--- remc2/engine/Basic.h
+++ remc2/engine/Basic.h
@@ -4,6 +4,7 @@
 #define MAIN_BASIC
 
 #include "Basic_mini.h"
+#include "Spells.h"
 
 #ifdef _MSC_VER
 #include <direct.h>  
@@ -245,54 +246,6 @@ typedef struct {
 	uint32_t var40_alloc_type;//28
 } Pathstruct;
 #pragma pack (16)
-/* ???
-Entity Sub-Type - Spell
-00 : Fireball
-01 : Heal
-02 : Speed Up
-03 : Posession
-04 : Shield
-05 : Beyond Sight
-06 : Earthquake
-07 : Meteor
-08 : Volcano
-09 : Crater
-0A : Teleport
-0B : Rubber Band
-0C : Invisible
-0D : Steal Mana
-0E : Rebound
-0F : Lightning
-10 : Castle
-11 : Skeleton
-12 : Thunderbolt
-13 : Mana Magnet
-14 : Fire Wall
-15 : Reverse Speed
-16 : Smart Bomb
-17 : Mini Fireball
-*/
-#pragma pack (1)
-typedef struct {//lenght 26
-	int32_t dword_2;
-	int32_t dword_6;
-	int32_t dword_A;
-	int32_t dword_E;
-	int32_t dword_0x12;
-	int16_t word_0x16x;
-	int16_t word_0x18;
-	int8_t byte_0x1A;
-	uint8_t byte_0x1B;
-}
-type_SPELLS_BEGIN_BUFFER_str_sub;
-
-typedef struct {//lenght 80
-	int8_t byte_0;
-	uint8_t byte_1;
-	type_SPELLS_BEGIN_BUFFER_str_sub subspell[3];
-}
-type_SPELLS_BEGIN_BUFFER_str;
-#pragma pack (16)
 
 #pragma pack (1)
 typedef struct {//lenght 10
@@ -491,7 +444,6 @@ extern Pathstruct xadatatables;
 
 extern axis_2d x_WORD_E3760_mouse;
 extern uint8_t* x_DWORD_E9C3C; // weak
-extern type_SPELLS_BEGIN_BUFFER_str SPELLS_BEGIN_BUFFER_str[];
 
 extern int16_t x_WORD_1806EE; // weak
 
---
+++ remc2/engine/Spells.h
@@ -0,0 +1,71 @@
+#pragma once
+
+#ifndef MAIN_SPELLS
+#define MAIN_SPELLS
+//#include "build_config.h"
+
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <string.h>
+//#include <ctype.h>
+#include <cstdint>
+//#include <functional>
+//#include <chrono>
+
+//#include "stdint.h"
+//#ifdef _MSC_VER
+//#include <windows.h>
+//#endif
+//#include "defs.h"
+//#include "global_types.h"
+
+/* ???
+Entity Sub-Type - Spell
+00 : Fireball
+01 : Heal
+02 : Speed Up
+03 : Posession
+04 : Shield
+05 : Beyond Sight
+06 : Earthquake
+07 : Meteor
+08 : Volcano
+09 : Crater
+0A : Teleport
+0B : Rubber Band
+0C : Invisible
+0D : Steal Mana
+0E : Rebound
+0F : Lightning
+10 : Castle
+11 : Skeleton
+12 : Thunderbolt
+13 : Mana Magnet
+14 : Fire Wall
+15 : Reverse Speed
+16 : Smart Bomb
+17 : Mini Fireball
+*/
+#pragma pack (1)
+typedef struct {//lenght 26
+	int32_t dword_2;
+	int32_t dword_6;
+	int32_t dword_A;
+	int32_t dword_E;
+	int32_t dword_0x12;
+	int16_t word_0x16x;
+	int16_t word_0x18;
+	int8_t byte_0x1A;
+	uint8_t byte_0x1B;
+}
+type_SPELLS_BEGIN_BUFFER_str_sub;
+
+typedef struct {//lenght 80
+	int8_t byte_0;
+	uint8_t byte_1;
+	type_SPELLS_BEGIN_BUFFER_str_sub subspell[3];
+}
+type_SPELLS_BEGIN_BUFFER_str;
+#pragma pack (16)
+extern type_SPELLS_BEGIN_BUFFER_str SPELLS_BEGIN_BUFFER_str[];
+#endif //MAIN_SPELLS
