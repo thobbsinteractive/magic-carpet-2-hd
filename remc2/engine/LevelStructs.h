37951eb76b49687fa9c8ae17313c820087e1aebe
Tomáš Veselý <turican0@gmail.com>
2025-12-22 15:03:59 +0100

    update LevelStructs

--- remc2/engine/LevelStructs.h
+++ remc2/engine/LevelStructs.h
@@ -22,6 +22,45 @@
 
 #include "BasicTerrain.h"
 #pragma pack (1)
+typedef struct {
+	uint8_t m_wReflections;
+	uint8_t m_wShadows;
+	uint8_t m_wSky;
+	uint8_t m_wViewPortSize;
+}
+GraphicsStruct_t;
+
+typedef struct {
+	uint8_t m_wMiniMap;//8590 icons//str_0x218E.str.icons_0x218E
+	uint8_t m_wTopBar;//8591 ??//str_0x218E.str.xxxx_0x218F
+	uint8_t m_uiScreenSize;//8592 screen size?//str_0x218E.str.scr_size_0x2190
+	uint8_t xxxx_0x2191;//8593//str_0x218E.str.xxxx_0x2191
+}
+DisplayStruct_t;
+
+typedef struct {
+	uint8_t xxxx_0x2192;//8594 ??//str_0x2192.str.xxxx_0x2192
+	uint8_t xxxx_0x2193;//8595 ??//str_0x2192.str.xxxx_0x2193
+	uint8_t m_wResolution;//8596 resolution//str_0x2192.str.resolution_0x2194
+	uint8_t shadows_0x2195;//8593 shadows//str_0x2192.str.shadows_0x2195
+}
+type_str_0x2192;
+
+typedef struct {
+	uint8_t m_wDynamicLighting;//8598 lights//str_0x2196.str.lights_0x2196
+	uint8_t setting_0x2197;//8599 setting//str_0x2196.str.setting_0x2197
+	uint8_t transparency_0x2198;//8600 transparency//str_0x2196.str.transparency_0x2198
+	uint8_t flat_0x2199;//8601 flat//str_0x2196.str.flat_0x2199
+}
+type_str_0x2196;
+
+typedef struct {
+	GraphicsStruct_t m_Graphics;//0x218A
+	DisplayStruct_t m_Display;//0x218E
+	type_str_0x2192 str_0x2192;
+	type_str_0x2196 str_0x2196;
+}
+GameSettingsStruct_t;
 
 #pragma pack (16)
 
--- remc2/engine/engine_support.h
+++ remc2/engine/engine_support.h
@@ -22,6 +22,7 @@
 #include "../portability/port_show_perifery.h"
 
 #include "BasicTerrain.h"
+#include "LevelStructs.h"
 
 #pragma pack (1)
 typedef struct {//lenght 3
@@ -891,81 +892,6 @@ typedef struct {//size 11
 typedef struct {//size 11 count 6
 	type_substr_3659C substr_3659C;
 } type_str_3659C;
-/*
-typedef struct {
-	int32_t dw;
-	int16_t w;
-} dw_ws;
-*/
-/*typedef struct {
-	int8_t Bit_0;
-	int8_t Bit_1;
-	int8_t Bit_2;
-	int8_t Bit_3;
-	int8_t Bit_4;
-	int8_t Bit_5;
-	int8_t Bit_6;
-	int8_t Bit_7;
-	int8_t Bit_8;
-	int8_t Bit_9;
-}
-typedef_str_0x6E3E;*/
-
-typedef struct {
-	uint8_t m_wReflections;
-	uint8_t m_wShadows;
-	uint8_t m_wSky;
-	uint8_t m_wViewPortSize;
-}
-GraphicsStruct_t;
-
-/*typedef union {
-	type_str_0x8586 str;
-	uint32_t dword;
-}
-type_uni_0x8586;*/
-
-typedef struct {
-	uint8_t m_wMiniMap;//8590 icons//str_0x218E.str.icons_0x218E
-	uint8_t m_wTopBar;//8591 ??//str_0x218E.str.xxxx_0x218F
-	uint8_t m_uiScreenSize;//8592 screen size?//str_0x218E.str.scr_size_0x2190
-	uint8_t xxxx_0x2191;//8593//str_0x218E.str.xxxx_0x2191
-}
-DisplayStruct_t;
-
-/*typedef union {
-	type_str_0x218E str;
-	uint32_t dword;
-}
-type_uni_0x218E;*/
-
-typedef struct {
-	uint8_t xxxx_0x2192;//8594 ??//str_0x2192.str.xxxx_0x2192
-	uint8_t xxxx_0x2193;//8595 ??//str_0x2192.str.xxxx_0x2193
-	uint8_t m_wResolution;//8596 resolution//str_0x2192.str.resolution_0x2194
-	uint8_t shadows_0x2195;//8593 shadows//str_0x2192.str.shadows_0x2195
-}
-type_str_0x2192;
-
-/*typedef union {
-	type_str_0x2192 str;
-	uint32_t dword;
-}
-type_uni_0x2192;*/
-
-typedef struct {
-	uint8_t m_wDynamicLighting;//8598 lights//str_0x2196.str.lights_0x2196
-	uint8_t setting_0x2197;//8599 setting//str_0x2196.str.setting_0x2197
-	uint8_t transparency_0x2198;//8600 transparency//str_0x2196.str.transparency_0x2198
-	uint8_t flat_0x2199;//8601 flat//str_0x2196.str.flat_0x2199
-}
-type_str_0x2196;
-
-/*typedef union {
-	type_str_0x2196 str;
-	uint32_t dword;
-}
-type_uni_0x2196;*/
 
 typedef struct {
 	uint8_t creflections_0x21AA;//uint8_t byte_0x21AA;//8618 can reflections//str_0x21AA.str.creflections_0x21AA
@@ -1045,14 +971,6 @@ typedef struct {//lenght 7
 	uint8_t stub[5];
 }
 type_str_0x6574;
-
-typedef struct {
-	GraphicsStruct_t m_Graphics;//0x218A
-	DisplayStruct_t m_Display;//0x218E
-	type_str_0x2192 str_0x2192;
-	type_str_0x2196 str_0x2196;
-}
-GameSettingsStruct_t;
 
 typedef struct {//lenght 224791
 	std::array<uint8_t, 4> stub0;
