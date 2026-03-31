43b8396bd67790994d0f74e5f251c65308d2cbbb
Tomaš Veselý <turican0@gmail.com>
2026-03-31 14:48:20 +0200

    update fcaseopen tests

--- remc2-unit-test/portability/fcaseopenUT.cpp
+++ remc2-unit-test/portability/fcaseopenUT.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 #include "../pch.h"
 #include "../../remc2/portability/fcaseopen.h"
+std::string g_exe_path;
 TEST (fcaseopen, tokens) {
 #ifdef __linux__
     std::string path1 = "/asdf/dummy/file.c";
@@ -26,9 +27,11 @@ TEST (fcaseopen, casepath) {
     ASSERT_EQ(casepath("/usr/share/"), "/usr/share/");
     ASSERT_EQ(casepath("/usr/../usr/share"), "/usr/../usr/share");
     ASSERT_EQ(casepath("/USR/SHARE"), "/usr/share");
-    ASSERT_EQ(casepath("remc2-unit-test"), "remc2-unit-test");
-    ASSERT_EQ(casepath("./remc2-unit-test"), "./remc2-unit-test");
-    ASSERT_EQ(casepath("./REMC2-unit-test"), "./remc2-unit-test");
+	std::string lower = g_exe_path;
+	std::transform(lower.begin(), lower.end(), lower.begin(),[](unsigned char c) { return std::tolower(c); });
+	std::string upper = g_exe_path;
+	std::transform(upper.begin(), upper.end(), upper.begin(),[](unsigned char c) { return std::toupper(c); });
+	ASSERT_EQ(casepath(upper), lower);
     ASSERT_EQ(casepath("./not_existing_file"), "./not_existing_file");
 #else //__linux__
     ASSERT_EQ(1, 1);
