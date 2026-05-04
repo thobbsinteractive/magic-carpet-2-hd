7c0747056fe641f946d766bd795a188e3c408843
Timothy Hobbs <thobbsinteractive@gmail.com>
2026-05-04 23:21:56 +0200

    fix: you can now have upto to 3x view distance

--- remc2/engine/GameRenderHD.cpp
+++ remc2/engine/GameRenderHD.cpp
@@ -54,22 +54,22 @@ void GameRenderHD::BuildTileRenderStepTable(TileStepQuadrant* table, int cols)
 	uint8_t pos_half = (uint8_t)(0x13 + (cols - 40) / 2);  // ~+cols/2
 
 	// Quadrant 0 (270->0)
-	table[0].startX = neg_half + 1;
-	table[0].startY = table[0].startY + 1;
+	table[0].startX = neg_half + (m_scaleViewDistance - 1);
+	table[0].startY = table[0].startY + (m_scaleViewDistance - 1);
 	table[0].rowStepX = neg_cols; 
 	table[0].rowStepY = 0xFF;
 	table[0].colStepX = 0x01;
 	table[0].colStepY = 0x00;
 
 	// Quadrant 1 (0->90)
-	table[1].startY = neg_half + 1;
+	table[1].startY = neg_half + (m_scaleViewDistance - 1);
 	table[1].rowStepX = 0x01;
 	table[1].rowStepY = neg_cols;
 	table[1].colStepX = 0x00;
 	table[1].colStepY = 0x01;
 
 	// Quadrant 2 (90->180)
-	table[2].startX = pos_half - 1;
+	table[2].startX = pos_half - (m_scaleViewDistance - 1);
 	table[2].startY = table[2].startY;
 	table[2].rowStepX = pos_cols;
 	table[2].rowStepY = 0x01;
@@ -77,8 +77,8 @@ void GameRenderHD::BuildTileRenderStepTable(TileStepQuadrant* table, int cols)
 	table[2].colStepY = 0x00;
 
 	// Quadrant 3 (180->270)
-	table[3].startX = table[3].startX + 1;
-	table[3].startY = pos_half - 1;
+	table[3].startX = table[3].startX + (m_scaleViewDistance - 1);
+	table[3].startY = pos_half - (m_scaleViewDistance - 1);
 	table[3].rowStepX = 0xFF;
 	table[3].rowStepY = pos_cols;
 	table[3].colStepX = 0x00;
