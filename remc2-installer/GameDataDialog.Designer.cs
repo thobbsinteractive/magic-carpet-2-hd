4e1c5a24c6f6bec2dd98abbeedaa9901f7e9b81d
Timothy Hobbs <thobbsinteractive@gmail.com>
2026-06-16 22:23:09 +0200

    fix: fixed more scaling issues

--- remc2-installer/GameDataDialog.Designer.cs
+++ remc2-installer/GameDataDialog.Designer.cs
@@ -42,18 +42,20 @@ namespace WixSharpSetup
             this.btnRun = new System.Windows.Forms.Button();
             this.cboInstallLocation = new System.Windows.Forms.ComboBox();
             this.lblInstallLocation = new System.Windows.Forms.Label();
-            this.txtPath = new System.Windows.Forms.TextBox();
             this.btnBrowse = new System.Windows.Forms.Button();
             this.lblGameData = new System.Windows.Forms.Label();
             this.grpBox = new System.Windows.Forms.GroupBox();
             this.btnInfo = new System.Windows.Forms.Button();
             this.treeViewInstallProgress = new System.Windows.Forms.TreeView();
             this.tblMain = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.txtPath = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.banner)).BeginInit();
             this.panel2.SuspendLayout();
             this.panel1.SuspendLayout();
             this.grpBox.SuspendLayout();
             this.tblMain.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // banner
@@ -141,39 +143,31 @@ namespace WixSharpSetup
             // 
             // cboInstallLocation
             // 
+            this.cboInstallLocation.Dock = System.Windows.Forms.DockStyle.Fill;
             this.cboInstallLocation.FormattingEnabled = true;
             this.cboInstallLocation.Items.AddRange(new object[] {
             "GOG Edition",
             "DOS Edition"});
-            this.cboInstallLocation.Location = new System.Drawing.Point(100, 17);
+            this.cboInstallLocation.Location = new System.Drawing.Point(203, 43);
             this.cboInstallLocation.Name = "cboInstallLocation";
-            this.cboInstallLocation.Size = new System.Drawing.Size(291, 31);
+            this.cboInstallLocation.Size = new System.Drawing.Size(254, 31);
             this.cboInstallLocation.TabIndex = 3;
             // 
             // lblInstallLocation
             // 
             this.lblInstallLocation.AutoSize = true;
-            this.lblInstallLocation.Location = new System.Drawing.Point(11, 20);
+            this.lblInstallLocation.Location = new System.Drawing.Point(3, 40);
             this.lblInstallLocation.Name = "lblInstallLocation";
             this.lblInstallLocation.Size = new System.Drawing.Size(148, 24);
             this.lblInstallLocation.TabIndex = 2;
             this.lblInstallLocation.Text = "Get Data From:";
             // 
-            // txtPath
-            // 
-            this.txtPath.Location = new System.Drawing.Point(15, 97);
-            this.txtPath.Margin = new System.Windows.Forms.Padding(3, 10, 3, 3);
-            this.txtPath.Name = "txtPath";
-            this.txtPath.Size = new System.Drawing.Size(381, 31);
-            this.txtPath.TabIndex = 5;
-            this.txtPath.TextChanged += new System.EventHandler(this.Path_TextChanged);
-            // 
             // btnBrowse
             // 
             this.btnBrowse.AutoSize = true;
-            this.btnBrowse.Location = new System.Drawing.Point(10, 49);
+            this.btnBrowse.Location = new System.Drawing.Point(3, 83);
             this.btnBrowse.Name = "btnBrowse";
-            this.btnBrowse.Size = new System.Drawing.Size(87, 35);
+            this.btnBrowse.Size = new System.Drawing.Size(148, 34);
             this.btnBrowse.TabIndex = 4;
             this.btnBrowse.Text = "Browse";
             this.btnBrowse.UseVisualStyleBackColor = true;
@@ -182,7 +176,8 @@ namespace WixSharpSetup
             // lblGameData
             // 
             this.lblGameData.AutoSize = true;
-            this.lblGameData.Location = new System.Drawing.Point(111, 55);
+            this.tableLayoutPanel1.SetColumnSpan(this.lblGameData, 2);
+            this.lblGameData.Location = new System.Drawing.Point(3, 0);
             this.lblGameData.Name = "lblGameData";
             this.lblGameData.Size = new System.Drawing.Size(280, 24);
             this.lblGameData.TabIndex = 15;
@@ -190,14 +185,11 @@ namespace WixSharpSetup
             // 
             // grpBox
             // 
-            this.grpBox.Controls.Add(this.lblGameData);
-            this.grpBox.Controls.Add(this.cboInstallLocation);
-            this.grpBox.Controls.Add(this.lblInstallLocation);
-            this.grpBox.Controls.Add(this.txtPath);
-            this.grpBox.Controls.Add(this.btnBrowse);
+            this.grpBox.Controls.Add(this.tableLayoutPanel1);
             this.grpBox.Dock = System.Windows.Forms.DockStyle.Fill;
             this.grpBox.Location = new System.Drawing.Point(3, 113);
             this.grpBox.Name = "grpBox";
+            this.grpBox.Padding = new System.Windows.Forms.Padding(10);
             this.grpBox.Size = new System.Drawing.Size(480, 50);
             this.grpBox.TabIndex = 17;
             this.grpBox.TabStop = false;
@@ -258,6 +250,38 @@ namespace WixSharpSetup
             this.tblMain.Size = new System.Drawing.Size(486, 336);
             this.tblMain.TabIndex = 20;
             // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 200F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.lblInstallLocation, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.lblGameData, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnBrowse, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.cboInstallLocation, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.txtPath, 0, 3);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(10, 34);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(460, 6);
+            this.tableLayoutPanel1.TabIndex = 16;
+            // 
+            // txtPath
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.txtPath, 2);
+            this.txtPath.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtPath.Location = new System.Drawing.Point(3, 130);
+            this.txtPath.Margin = new System.Windows.Forms.Padding(3, 10, 3, 3);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(454, 31);
+            this.txtPath.TabIndex = 5;
+            this.txtPath.TextChanged += new System.EventHandler(this.Path_TextChanged);
+            // 
             // GameDataDialog
             // 
             this.AcceptButton = this.btnRun;
@@ -279,9 +303,10 @@ namespace WixSharpSetup
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.grpBox.ResumeLayout(false);
-            this.grpBox.PerformLayout();
             this.tblMain.ResumeLayout(false);
             this.tblMain.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -296,7 +321,6 @@ namespace WixSharpSetup
         private System.Windows.Forms.Button btnRun;
         private System.Windows.Forms.ComboBox cboInstallLocation;
         private System.Windows.Forms.Label lblInstallLocation;
-        private System.Windows.Forms.TextBox txtPath;
         private System.Windows.Forms.Button btnBrowse;
         private System.Windows.Forms.Button btnNext;
         private System.Windows.Forms.Label lblGameData;
@@ -304,5 +328,7 @@ namespace WixSharpSetup
 		private System.Windows.Forms.Button btnInfo;
 		private System.Windows.Forms.TreeView treeViewInstallProgress;
 		private System.Windows.Forms.TableLayoutPanel tblMain;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+		private System.Windows.Forms.TextBox txtPath;
 	}
 }
