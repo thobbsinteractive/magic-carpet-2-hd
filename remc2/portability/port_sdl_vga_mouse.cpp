3b88190077cfea1d713cc1dc1f4f4089a23cf104
Timothy Hobbs <thobbsinteractive@gmail.com>
2026-07-25 10:39:57 +0200

    fix: added boundy check the scale down of coords in menu movement

--- remc2/portability/port_sdl_vga_mouse.cpp
+++ remc2/portability/port_sdl_vga_mouse.cpp
@@ -1007,13 +1007,21 @@ void ScaleDownMouseCoordsToVga(int16_t& x, int16_t& y)
 {
 	if (m_iOrigw > 640)
 	{
-		float fx = (640.0f * mouseScaleX) / (float)m_iOrigw;
+		float fx = 1;
+
+		if ((640.0f * mouseScaleX) <= m_iOrigw)
+			fx = (640.0f * mouseScaleX) / (float)m_iOrigw;
+
 		x = fx * x;
 	}
 
 	if (m_iOrigh > 480)
 	{
-		float fy = (480.0f * mouseScaleY) / (float)m_iOrigh;
+		float fy = 1;
+
+		if ((480.0f * mouseScaleY) <= m_iOrigh)
+			fy = (480.0f * mouseScaleY) / (float)m_iOrigh;
+
 		y = fy * y;
 	}
 }
