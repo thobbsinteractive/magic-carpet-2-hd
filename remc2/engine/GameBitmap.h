59ab3e9e1b2ee845b0d825cb2187111c44b38f14
Timothy Hobbs <thobbsinteractive@gmail.com>
2026-08-14 22:13:59 +0200

    feat: added methods to convert pallet to Rgb

--- remc2/engine/GameBitmap.h
+++ remc2/engine/GameBitmap.h
@@ -15,6 +15,9 @@ public:
 	static void DrawTransparentBitmap_2DE80(int16_t posX, int16_t posY, bitmap_pos_struct_t a3, uint8_t scale = 1);
 	static void DrawMenuGraphic(uint16_t width, uint16_t height, uint8_t scale, uint8_t* ptrSrc, uint8_t* ptrDest);
 	static void ScaleMenuGraphic(uint16_t height, uint8_t scale, uint8_t* ptrSrc, uint8_t* ptrDest);
+	static void PaletteToRgb(TColor* ptrPalette, uint8_t colorAddressIdx, uint8_t truColorOut[3]);
+	static void PaletteToRgba(TColor* ptrPalette, uint8_t colorIdx, uint8_t truColorOut[4]);
+	static uint8_t DeriveBlendAlpha(TColor* ptrPalette, uint8_t srcIndex, uint8_t dstIndex, uint8_t resultIndex);
 };
 
 #endif //GAME_BITMAP
